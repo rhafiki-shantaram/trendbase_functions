cd62ec66a04e8a35ba4b5ef14b45c55069c2bc8e
rhys_e <rhys.ewins@live.com.au>
2023-12-31 19:47:14 +0800

    deleted extension disable

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const scrapeLogic = async(res) => {
     const browser = await puppeteer.launch({
         headless: "new",
-        ignoreDefaultArgs: ['--disable-extensions'],
         args: [
             "--disable-setuid-sandbox",
             "--no-sandbox",
