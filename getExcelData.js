4caceea9f389a2011b71743d911d592305a7fda4
rhys_e <rhys.ewins@live.com.au>
2024-02-14 15:33:18 +0800

    Add getExcelData.js

--- getExcelData.js
+++ getExcelData.js
@@ -1,28 +1,30 @@
 const axios = require('axios');
 const ExcelJS = require('exceljs');
 
-// Function to get Excel content using axios
+// Enhanced function to get Excel content using axios
 async function getExcelContent(url, sheetName) {
-    return axios({
-        url,
-        method: 'GET',
-        responseType: 'arraybuffer' // Important for handling binary data like Excel files
-    })
-    .then(async response => {
+    try {
+        const response = await axios({
+            url,
+            method: 'GET',
+            responseType: 'arraybuffer', // Important for handling binary data like Excel files
+            // Timeout can be adjusted or removed based on your requirements
+            timeout: 30000, // Timeout after 30 seconds
+        });
+
         const workbook = new ExcelJS.Workbook();
         await workbook.xlsx.load(response.data); // response.data is an ArrayBuffer
-        
+
         const worksheet = workbook.getWorksheet(sheetName);
         if (!worksheet) throw new Error("Worksheet not found");
 
         let headers = [];
         let rows = [];
-        worksheet.eachRow(function (row, rowNumber) {
+        worksheet.eachRow((row, rowNumber) => {
             if (rowNumber === 1) {
                 headers = row.values.slice(1);
                 rows.push(row.values.slice(1)); // Add headers as the first row in rows
             } else {
-                // Store all rows
                 rows.push(row.values.slice(1));
             }
         });
@@ -37,17 +39,17 @@ async function getExcelContent(url, sheetName) {
         });
 
         return json;
-    })
-    .catch(error => {
-        throw new Error(`Failed to fetch Excel file: ${error.message}`);
-    });
+    } catch (error) {
+        // Enhanced error handling
+        console.error(`Failed to fetch or process Excel file: ${error}`);
+        throw new Error(`Failed to fetch or process Excel file: ${error.message}`);
+    }
 }
 
 // Function to be used in your Express route
 async function getExcelData(req, res) {
     // Retrieve URL and sheetName from query parameters
     const { url, sheetName } = req.query;
-    console.log(`qParams: `, { url, sheetName });
 
     // Validate input
     if (!url || !sheetName) {
